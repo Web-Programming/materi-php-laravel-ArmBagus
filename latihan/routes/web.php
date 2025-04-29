dd3ace57982f40e3e1c253d907e0f0d212e29ece
ArmBagus <bagusaufaariqarmeidi_2428250059@mhs.mdp.ac.id>
2025-04-29 15:03:17 +0700

    view laravel

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profil', function (): view {
+    return view('profil');
+});
+
+Route::get('/berita/{id}', function ($id): view {
+    return view('berita'), ['id' => $id]; 
+});
+
+Route::get('/total/{angka1}/{angka2}', function ($angka1, $angka2): view {
+    $total = $angka1 + $angka2;
+    return view('hasil'), ['total' => $total];
+});
